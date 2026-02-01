7e903406b12950984f6725d3c0bc4529040b3b5e
Nakul Sharma <sharmanakul00734@gmail.com>
2026-02-01 13:35:11 +0530

    inital commit

---
+++ src/main/java/com/ai_integration/gemini/service/ChatService.java
@@ -0,0 +1,18 @@
+package com.ai_integration.gemini.service;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatService {
+
+    private final ChatClient chatClient;
+
+    public ChatService(ChatClient.Builder chatClient){
+        this.chatClient = chatClient.build();
+    }
+
+    public String ask(String prompt){
+        return chatClient.prompt(prompt).call().content();
+    }
+}
